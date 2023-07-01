b0d5094d0cfe7b2b5ab6ee029df50b7071a9ae29
Bird-Forest <bird.forest70@gmail.com>
2023-07-01 03:47:42 +0300

    2023.07.01-1

--- js/task-07.js
+++ js/task-07.js
@@ -8,16 +8,3 @@ function onInput(evt) {
 	let step = document.querySelector('#font-size-control').value;
 	 textSpan.style.fontSize = `${step}px`;
 }
-
-
-// function fun1() {
-// 	var rng=document.getElementById('r1'); //rng - это Input
-// 	var p=document.getElementById('one'); // p - абзац
-// 	p.innerHTML=rng.value;
-// }
-
-// document.querySelector('.inpput-1').addEventListener('input', () => {
-// 	let  data = document.querySelector('.input-1').value;
-// 	document.querySelector('.out-5').innerHTML = data;
-// });
-// fontSize.style.size = `${step}px`;
