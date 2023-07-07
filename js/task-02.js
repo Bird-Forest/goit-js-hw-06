43283583c08d5dbb72b99cdf341eb126f5c8ec45
Bird-Forest <bird.forest70@gmail.com>
2023-07-07 15:43:19 +0300

    2023.07.07- остаточний

--- js/task-02.js
+++ js/task-02.js
@@ -10,17 +10,21 @@ const ingredients = [
 const ingredientsList = document.querySelector('#ingredients')
 console.dir(ingredientsList);
 
-function addElements(ingredients) {
 
-  for (let i = 0; i < ingredients.length; i+=1) {
-    const ingredient = document.createElement('li');
-    ingredient.textContent = ingredients[i];
-    ingredient.classList.add('item');
-    ingredientsList.append(ingredient);
-  };
-}
+ingredients.map((elem, idx) => {
+  const ingredient = document.createElement('li');
+  ingredient.textContent = ingredients[idx];
+  ingredient.classList.add('item');
+  console.dir(ingredient);
+  // return ingredient
+  // Мій варіант
+  ingredientsList.append(ingredient); 
+});
+  // розпилення
+// ingredientsList.append(...ingredient); 
 
-addElements(ingredients);
+
+
 
 // function addElements(ingredients) {
 
